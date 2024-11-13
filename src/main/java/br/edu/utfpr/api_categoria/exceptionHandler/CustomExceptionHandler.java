851083ea9fdcc946a5a9b4e4f0156d01286cb4ea
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2024-11-12 21:37:51 -0300

    handler

---
+++ src/main/java/br/edu/utfpr/api_categoria/exceptionHandler/CustomExceptionHandler.java
@@ -0,0 +1,21 @@
+package br.edu.utfpr.api_categoria.exceptionHandler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<HashMap<String, String>> exceptionHandler(MethodArgumentNotValidException e) {
+        HashMap<String, String> errors = new HashMap<>();
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
